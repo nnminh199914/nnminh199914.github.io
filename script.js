3e5dff5bbddb6020c383f8db92cf51383c54ea69
nnminh199914 <30563683+nnminh199914@users.noreply.github.com>
2022-03-26 16:56:53 +0100

    updated

--- script.js
+++ script.js
@@ -63,7 +63,8 @@ let engine = Matter.Engine.create();
         
         
         ///
-        Matter.World.add(engine.world,[boxA,boxB,mouseConstraint,Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
+        Matter.World.add(engine.world,[boxA,boxB,mouseConstraint,
+            Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
             Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
             Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
             Bodies.rectangle(0, 300, 50, 600, { isStatic: true })]);
