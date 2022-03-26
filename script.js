f37ccc57993e8e5c6072b3b8d6b7cace1439375a
nnminh199914 <30563683+nnminh199914@users.noreply.github.com>
2022-03-26 16:48:17 +0100

    try git web

---
+++ script.js
@@ -0,0 +1,69 @@
+
+let engine = Matter.Engine.create();
+        let render = Matter.Render.create({
+            element: document.body.firstElementChild,
+            engine:engine ,
+            options: {
+                width: innerWidth,
+                height: innerHeight,
+                wireframes: false,
+                background: "#C5D8A4"
+            }
+        });
+        ////
+         // add mouse control
+        let mouse = Matter.Mouse.create(render.canvas);
+        let mouseConstraint = Matter.MouseConstraint.create(engine, {
+            mouse: mouse,
+            constraint: {
+            render: {visible: false}
+        }
+        });
+        render.mouse = mouse;
+        //parms: x,y,width,height,options
+        //let ground = Matter.Bodies.rectangle(400,600,1000,60,{ isStatic: true}); 
+        let boxA = Matter.Bodies.rectangle(400,200,80,80,{
+            render: {
+                fillStyle: "#534340"
+            }
+        });
+        let boxB = Matter.Bodies.rectangle(450,50,230,70,{
+                render: {
+                    strokeStyle: '#ffffff',
+                    sprite: {
+                        texture: 'box.png',
+                        xScale: 1,
+                        yScale: 1
+                    }
+                }
+            });
+        ///
+        if (typeof window !== 'undefined') {
+        var updateGravity = function(event) {
+            var orientation = typeof window.orientation !== 'undefined' ? window.orientation : 0,
+                gravity = engine.gravity;
+
+            if (orientation === 0) {
+                gravity.x = Matter.Common.clamp(event.gamma, -90, 90) / 90;
+                gravity.y = Matter.Common.clamp(event.beta, -90, 90) / 90;
+            } else if (orientation === 180) {
+                gravity.x = Matter.Common.clamp(event.gamma, -90, 90) / 90;
+                gravity.y = Matter.Common.clamp(-event.beta, -90, 90) / 90;
+            } else if (orientation === 90) {
+                gravity.x = Matter.Common.clamp(event.beta, -90, 90) / 90;
+                gravity.y = Matter.Common.clamp(-event.gamma, -90, 90) / 90;
+            } else if (orientation === -90) {
+                gravity.x = Matter.Common.clamp(-event.beta, -90, 90) / 90;
+                gravity.y = Matter.Common.clamp(event.gamma, -90, 90) / 90;
+            }
+        };
+
+        window.addEventListener('deviceorientation', updateGravity);
+        }
+        
+        
+        ///
+        Matter.World.add(engine.world,[boxA,boxB,mouseConstraint]);
+        Matter.Engine.run(engine);
+        Matter.Render.run(render);
+        console.log(render.canvas.parentNode)
