009f6f049a8600205378abf0bc13cd117e8ab7d4
nnminh199914 <30563683+nnminh199914@users.noreply.github.com>
2022-03-27 01:20:16 +0100

    updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 let engine = Matter.Engine.create();
         let render = Matter.Render.create({
-            element: document.body.firstElementChild,
+            element: document.body.firstElementChild.firstElementChild,
             engine:engine ,
             options: {
                 width: 800,
